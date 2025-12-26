2fa9dd68a660c3824663472073e0ac2056605123
Ethan <jcwork.chan@gmail.com>
2025-12-26 16:32:07 +0800

    add pmd ruleset and exclude jacoco branch

--- src/main/java/c2c/payment/PaymentRepository.java
+++ src/main/java/c2c/payment/PaymentRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface PaymentRepository {
     Payment save(Payment payment);
 
-    Optional<Payment> findByOrderId(String orderId);
+//    Optional<Payment> findByOrderId(String orderId);
 
     List<Payment> findAll();
 }
